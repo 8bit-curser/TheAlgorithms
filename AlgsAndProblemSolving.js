e8255e5ef9fe61a243713eadd401b608827939d6
Tom <tomas@humminghomes.com>
2022-05-03 09:45:57 -0300

    Started with problem solving

---
+++ AlgsAndProblemSolving.js
@@ -0,0 +1,50 @@
+// Alg definition:
+//
+//  Set of steps to accomplish a task
+
+
+// 
+// === "Fool proof" steps to problem solving: ===
+// 
+// 1. Understand the problem
+// 2. Explore concrete examples
+// 3. Break it down
+// 4. Solve it/ Simplify
+// 5. Look back and refactor
+// 
+// ===============================================
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
